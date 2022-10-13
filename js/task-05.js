b9f2771f98d261b91c278d5cef1e0c352df63172
Yuri Belebekha <ybelebekha@gmail.com>
2022-10-13 21:20:26 +0300

    finished Task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const refs = {
+  nameInput: document.querySelector('#name-input'),
+  nameOutput: document.querySelector('#name-output'),
+};
+
+refs.nameInput.addEventListener('input', onInputChange);
+
+function onInputChange (event) {   
+  refs.nameOutput.textContent = event.currentTarget.value;
+};
