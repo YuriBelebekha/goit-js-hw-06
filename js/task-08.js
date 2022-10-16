4a756a8a4006627dce9a866b4e65c3ed2545bce5
Yuri Belebekha <ybelebekha@gmail.com>
2022-10-16 21:00:33 +0300

    finished Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginFormRef = document.querySelector('.login-form');
+
+loginFormRef.addEventListener('submit', onLoginFormSubmit);
+
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+
+  if (event.currentTarget.elements.email.value == ''
+  || event.currentTarget.elements.password.value == '') {
+    alert('Всі поля повинні бути заповнені');
+  } else {
+    const logins = {};
+    logins.email = event.currentTarget.elements.email.value;
+    logins.password = event.currentTarget.elements.password.value;
+
+    console.log(logins);  
+    loginFormRef.reset();
+  }
+};
 
