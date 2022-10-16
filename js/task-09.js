9cca9bbcdfde2b140e2db886bd3c05bb572a0d41
Yuri Belebekha <ybelebekha@gmail.com>
2022-10-16 21:54:16 +0300

    finished Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const btnRef = document.querySelector('.change-color');
+const bgColorHex = document.querySelector('.color');
+
+btnRef.addEventListener('click', onClickChangeBodyColor);
+
+function onClickChangeBodyColor () {
+  const randomHexColor = getRandomHexColor();
+
+  bodyRef.style.backgroundColor = randomHexColor;  
+  bgColorHex.textContent = randomHexColor;  
+}
